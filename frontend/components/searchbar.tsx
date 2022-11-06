40b8c32342a4f49d16c0e1726ff4355747440315
IkaWaAyuMu <pungpond4450@gmail.com>
2022-11-07 01:47:52 +0700

    Remove unnescessary variables

--- frontend/components/searchbar.tsx
+++ frontend/components/searchbar.tsx
@@ -1,29 +1,25 @@
-import { useCallback, useRef, useState } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 
 export default function Searchbar() {
-    const searchRef = useRef(null)
     const [query, setQuery] = useState('')
     const router = useRouter()
 
-    const onChange = useCallback((e) => {
-        setQuery(e.target.value)
-    }, [])
-
     const onSubmit = (e) => {
+        router.route = '/'
         router.query.search = query
         router.push(router)
         e.preventDefault();
     }
 
     return (
-        <form className='w-1/2 bg-slate-800 h-full' ref={searchRef} onSubmit={onSubmit}>
+        <form className='w-1/2 bg-slate-800 h-full' onSubmit={onSubmit}>
             <input
                 className='w-3/4 p-1'
                 type='search'
                 placeholder='Search...'
                 value={query}
-                onChange={onChange}
+                onChange={e => setQuery(e.target.value)}
             />
             <input className='w-1/4 bg-sky-700 hover:bg-sky-400 text-white p-1' type="submit" value="Search"/>
         </form >
