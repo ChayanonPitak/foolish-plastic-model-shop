26151a4aecaa1214f6f86c04582cdcad6804cd59
thegenggo <kritsanaphong-keang@hotmail.com>
2022-11-06 19:07:16 +0700

    add on-off navbar components

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,12 +1,17 @@
 import Searchbar from "./searchbar"
 import NavAuth from "./navauth"
 
-export default function Navbar() {
+type NavbarProps = {
+  searchbar?: boolean;
+  navauth?: boolean;
+};
+
+export default function Navbar({searchbar, navauth}: NavbarProps) {
   return (
     <div className="w-full bg-slate-800 h-min p-7 flex justify-between">
         <a className="text-3xl text-white" href="."> Foolish Shop </a>
-        <Searchbar/>
-        <NavAuth/>
+        {searchbar ? <Searchbar/> : null}
+        {navauth ? <NavAuth/> : null}
     </div>
   )
 }
