9767511d960bd2231d4e1de08b55e9b4385dca59
BottlesMan <man2545_@hotmail.com>
2022-11-06 20:29:52 +0700

    Add office model

---
+++ api-server/app/Models/Office.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Office extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $table = 'offices';
+    protected $primaryKey = 'officeCode';
+    protected $fillable = [
+        'officeCode',
+        'city',
+        'phone',
+        'addressLine1',
+        'addressLine2',
+        'state',
+        'country',
+        'postalCode',
+        'territory',
+    ];
+}
