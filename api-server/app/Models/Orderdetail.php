c26789c3efba039e2e90ec3dfcd53d83213976f5
BottlesMan <man2545_@hotmail.com>
2022-11-06 20:37:30 +0700

    Add orderdetails model

---
+++ api-server/app/Models/Orderdetail.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orderdetail extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $table = 'orderdetails';
+    protected $primaryKey = ["orderNumber","productCode"];
+    protected $fillable = [
+        'orderNumber',
+        'productCode',
+        'quantityOrdered',
+        'priceEach',
+        'orderLineNumber',
+    ];
+}
