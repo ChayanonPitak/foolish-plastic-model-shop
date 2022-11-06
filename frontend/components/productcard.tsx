76f5f98990aff26a2c7184454d80ce0ddecf3741
DifficultIV <kitpisan@gmail.com>
2022-11-07 03:34:56 +0700

    add productcard on cart prototype

--- frontend/components/productcard.tsx
+++ frontend/components/productcard.tsx
@@ -1,14 +1,15 @@
 type ProductProps = {
-    name?: String;
-    price?: Number;
-    stock?: Number;
+    name?: string;
+    price?: number;
+    stock?: number;
+    img?: string;
 };
 
-export default function ProductCard({name,price,stock}: ProductProps) {
+export default function ProductCard({name,price,stock,img}: ProductProps) {
 
     return (
         <div className="p-7 ">
-            <div className="w-72 h-48 object-scale-down"><img src="https://ford-mustang-thailand.com/assets/images/cars/FORD-MUSTANG-GT.jpg?111"/></div>
+            <div className="w-72 h-48 object-scale-down ring-2 ring-black"><img src={img} /></div>
             <div>{name}</div>
                 <div className=" flex space-x-40">
                     <p>${price}</p>
---
+++ frontend/components/productcard_cart.tsx
@@ -0,0 +1,26 @@
+type ProductProps = {
+    name?: string;
+    price?: number;
+    order?: number;
+    img?: string;
+};
+
+export default function ProductCart({name,price,order,img}: ProductProps) {
+    return (
+        <div className="p-7 flex space-x-4">
+            <div className="object-fill w-24 h-24 ring-2 ring-black"><img src={img} /></div>
+                <div>
+                    <p>{name}</p>
+                    <p>${price}/piece</p>
+                    <p>Total: {order*price}</p>
+                </div>
+                <div className="flex space-x-4">
+                    <button className='w-8 h-8 bg-sky-700 hover:bg-sky-400 text-white p-1'>-</button>
+                    <p>Order: {order}</p>
+                    <button className='w-8 h-8 bg-sky-700 hover:bg-sky-400 text-white p-1'>+</button>
+                </div>
+                <div><button className='w-16 h-8 bg-sky-700 hover:bg-sky-400 text-white p-1'>Delete</button></div>
+                
+        </div>
+    )
+}
