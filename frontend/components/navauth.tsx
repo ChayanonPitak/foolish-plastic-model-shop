e1a6d92b751f38729326b9cd16b6c036e550bb1f
IkaWaAyuMu <pungpond4450@gmail.com>
2022-11-07 02:33:32 +0700

    Add register to navbar

--- frontend/components/navauth.tsx
+++ frontend/components/navauth.tsx
@@ -9,16 +9,23 @@ export default function NavAuth() {
         if (typeof window !== 'undefined') token = UserToken.getToken()
     }, [])
     
+    const logOut = () => UserToken.setToken('')
 
     return (
         <div className='w-1/6 h-full'>
             { !token && 
-                <Link href="/login">
-                    <button className='w-full bg-sky-700 hover:bg-sky-400 text-white p-1' type="button">Log in</button>
-                </Link> 
+                <div className="flex justify-end">
+                    <Link href="/login">
+                        <button className='w-full bg-sky-700 hover:bg-sky-400 text-white p-1' type="button">Log in</button>
+                    </Link> 
+                    <div className='w-1'></div>
+                    <Link href="/register">
+                        <button className='w-full bg-sky-700 hover:bg-sky-400 text-white p-1' type="button">Register</button>
+                    </Link> 
+                </div>
             }
             { token && 
-                <div className="flex">
+                <div className="flex ">
                     <Link href="/cart">
                         <button className='max-w-md bg-sky-700 hover:bg-sky-400 text-white p-1' type="button">Cart</button>
                     </Link> 
@@ -26,6 +33,8 @@ export default function NavAuth() {
                     <Link href="/account">
                         <button className='max-w-md bg-sky-700 hover:bg-sky-400 text-white p-1' type="button">Account</button>
                     </Link> 
+                    <div className='w-1'></div>
+                    <button className='max-w-md bg-sky-700 hover:bg-sky-400 text-white p-1' type="button" onClick={logOut}>Log out</button>
                 </div>
             }
         </div>
