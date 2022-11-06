a16978691bee9091810619d6ec2013a1577d6bfe
BottlesMan <man2545_@hotmail.com>
2022-11-06 20:39:20 +0700

    Add payment model

---
+++ api-server/app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $table = 'payments';
+    protected $primaryKey = ["customerNumber","checkNumber"];
+    protected $fillable = [
+        'customerNumber',
+        'checkNumber',
+        'paymentDate',
+        'amount',
+    ];
+}
