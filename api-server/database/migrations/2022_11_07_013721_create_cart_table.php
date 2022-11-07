d076aca38ff2573fb1dbe8aae43c9a0203625579
thegenggo <kritsanaphong-keang@hotmail.com>
2022-11-07 11:05:30 +0700

    add cart migration

--- api-server/database/migrations/2022_11_07_013721_create_cart_table.php
+++ api-server/database/migrations/2022_11_07_013721_create_cart_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger("userId")->length(20)->unsigned();
             $table->string("productCode", 15)->collation("latin1_swedish_ci");
-            $table->smallInteger("quantityInStock")->length(6);
+            $table->smallInteger("quantity")->length(6);
             $table->foreign("userId")->references("id")->on("users")->onDelete("cascade");
             $table->foreign("productCode")->references("productCode")->on("products")->onDelete("cascade");
         });
